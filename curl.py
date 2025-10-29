435e591b51d9c550e7bc39a6deff15a3d9133b16
Sasha Shkoda <shkoda_sashka@mail.ru>
2025-10-29 21:30:15 +0300

    Созданы заготовки файлов

---
+++ curl.py
@@ -0,0 +1,4 @@
+class url():
+    main_page = 'https://qa-scooter.praktikum-services.ru' #главная страница сервиса
+    order_page = f'{main_page}/order' #страница оформления заказа
+    dzen_page = 'https://dzen.ru/?yredirect=true' #главная страница Дзена
